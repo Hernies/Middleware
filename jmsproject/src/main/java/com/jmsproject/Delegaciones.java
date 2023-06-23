a0bd17ee0cfdd61ed68fa10b87072b016655d3e1
Hernies <hernancalvoaguiar.2@gmail.com>
2023-06-23 18:45:26 +0200

    jiji

---
+++ jmsproject/src/main/java/com/jmsproject/Delegaciones.java
@@ -0,0 +1,15 @@
+package com.jmsproject;
+import java.util.*;
+import javax.jms.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Delegaciones 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
