bacf4df728fabf39741e20766d19f3915b54a6ec
Hernies <hernancalvo>
2023-06-26 18:53:28 +0200

    Falta limpiar el procesado de datos

--- jmsproject/src/main/java/com/jmsproject/Inmueble.java
+++ jmsproject/src/main/java/com/jmsproject/Inmueble.java
@@ -1,61 +1,127 @@
 package com.jmsproject;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.Topic;
+
 public class Inmueble {
-    // crear constructor de inmueble con distrito,precio,numero de dormitorios,numero de baños y superficie
-    private String delegacion;
-    private String distrito;
-    private int precio;
-    private int numeroDormitorios;
-    private int numeroBanos;
-    private int superficie;
-    private int preciom2;
+    private String fichero;
+	private String delegacion;
+	private int nOficina;
+	private Session sesion;
+    private ArrayList<Oficina> oficinas = new ArrayList<Oficina>();
+    public MessageProducer msgProducer;
+	public Topic myTopic;
+
+    public Inmueble(String fichero, Session sesion) throws JMSException, IOException{
+        this.fichero = fichero;
+        if (fichero.startsWith("Centro")){
+            this.delegacion = "Centro";
+            myTopic = sesion.createTopic("Centro");
+        } else if (fichero.startsWith("Norte")){
+            this.delegacion = "Norte";
+            myTopic = sesion.createTopic("Norte");
+        } else if (fichero.startsWith("Sur")){
+            this.delegacion = "Sur";
+            myTopic = sesion.createTopic("Sur");
+        } else if (fichero.startsWith("Este")){
+            this.delegacion = "Este";
+            myTopic = sesion.createTopic("Este");
+        } else if (fichero.startsWith("Oeste")){
+            this.delegacion = "Oeste";
+            myTopic = sesion.createTopic("Oeste");
+        }
+        this.sesion = sesion;
+        System.out.println(fichero);
+        msgProducer = sesion.createProducer(myTopic);
+        procesar();
+        enviar();
+    }
+
+    private void enviar() throws JMSException {
+        ObjectMessage ofiMessage = sesion.createObjectMessage();
+        ofiMessage.setObject(oficinas);
+        ofiMessage.setStringProperty("Delegacion", delegacion);
+        System.out.println("Enviando mensaje a " + delegacion);
+        msgProducer.send(ofiMessage);
+    }
+
+    // TODO ARREGLAR PROCESAR
+    private void procesar() throws IOException { 
+        BufferedReader rdr = new BufferedReader (new FileReader ("../src/Ficheros_Practica_1/"+fichero));
+		String strCurrentLine;
+		while ((strCurrentLine = rdr.readLine()) != null) {
+            Oficina oficina;
+			String distrito = " ";
+			String precio = "";
+			String nHab=" ";
+			String nBan=" ";
+            String superficie=" ";
+			String [] parts = strCurrentLine.split(",");            
+            for(int i = 0; i< parts.length; i++){
+                System.out.println(parts[i]);
+            }
+            if(parts.length == 1){
+                distrito = parts[0];
+            }
+            if(parts.length == 2){
+                distrito = parts[0];
+                precio = parts[1];
+            }
+            if(parts.length == 3){
+                distrito = parts[0];
+                precio = parts[1];
+                nHab = parts[2];
+            }
+            if(parts.length == 4){
+                distrito = parts[0];
+                precio = parts[1];
+                nHab = parts[2];
+                nBan = parts[3];
+            }
+            if(parts.length == 5){
+                distrito = parts[0];
+                precio = parts[1];
+                nHab = parts[2];
+                nBan = parts[3];
+                superficie = parts[4];
+            }
+
+			oficina = new Oficina(distrito, precio, nHab, nBan, superficie);
+            System.out.println("\n Ofi"+oficina.toString()+"\n");
+            oficinas.add(oficina);
+        }
+		rdr.close();
+    }
+
+    //main function
+    public static void main(String[] args) throws JMSException {
+        try{
+            ConnectionFactory myConnFactory = new com.sun.messaging.ConnectionFactory();
+            Connection myConn = myConnFactory.createConnection();
+            Session mySesion = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            // MessageProducer producer = mySesion.createProducer();
+            String[] path = new File("../src/Ficheros_Practica_1").list();
+            for(String pathname : path){
+                 new Inmueble(pathname, mySesion);
+                 Thread.sleep(5000);
+            }    
+            mySesion.close();
+            myConn.close();
+        } catch (Exception jsme){
+            jsme.printStackTrace();
+        }
+    }
     
-    //Constructor
-    public Inmueble(String delegacion, String distrito, int precio, int numeroDormitorios, int numeroBanos, int superficie) {
-        this.delegacion = delegacion;
-        this.distrito = distrito;
-        this.precio = precio;
-        this.numeroDormitorios = numeroDormitorios;
-        this.numeroBanos = numeroBanos;
-        this.superficie = superficie;
-    }
-
-    public Inmueble(String delegacion, String distrito, int precio, int numeroDormitorios, int numeroBanos, int superficie, int preciom2) {
-        this.delegacion = delegacion;
-        this.distrito = distrito;
-        this.precio = precio;
-        this.numeroDormitorios = numeroDormitorios;
-        this.numeroBanos = numeroBanos;
-        this.superficie = superficie;
-        this.preciom2 = preciom2;
-    }
-
-    //getters 
-    public String getDelegacion() {
-        return delegacion;
-    }
-
-    public String getDistrito() {
-        return distrito;
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public int getNumeroDormitorios() {
-        return numeroDormitorios;
-    }
-
-    public int getNumeroBanos() {
-        return numeroBanos;
-    }   
-
-    public int getSuperficie() {
-        return superficie;
-    }
-
-    public int getPreciom2() {
-        return preciom2;
-    }
 }
