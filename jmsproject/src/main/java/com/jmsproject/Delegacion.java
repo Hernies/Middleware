fd9fd3ff0ce8a42eda10ee315a5bd25e2224f143
Hernies <hernancalvo>
2023-06-26 18:59:12 +0200

    arreglando el warning de unchecked cast

--- jmsproject/src/main/java/com/jmsproject/Delegacion.java
+++ jmsproject/src/main/java/com/jmsproject/Delegacion.java
@@ -1,66 +1,62 @@
 package com.jmsproject;
+import java.io.Serializable;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.sun.messaging.ConnectionConfiguration;
-import javax.jms.*;
-
-public class Delegacion {
-    private String nombre;
-    private List<Oficina> oficinas;
-    private EquipoGestion equipoGestion;
-    private ArrayList<Inmueble> inmueblesCambiados;
-
-    public Delegacion(String nombre, List<Oficina> oficinas, EquipoGestion equipoGestion) {
-        this.nombre = nombre;
-        this.oficinas = new ArrayList<>();
-        this.equipoGestion = equipoGestion;
+public class Delegacion implements Serializable {
+    //Delegacion attributes: precio, nHab, nBan, superficie, delegacion, distrito
+    private int precio;
+    private int nHab;
+    private int nBan;
+    private int superficie;
+    private float precioM2;
+    private String distrito;
+    
+    //constructor for Delegacion and its attributes, adding it to the Delegaciones hashmap
+    public Delegacion(int precio, int nHab, int nBan, int superficie, float precioM2, String delegacion, String distrito){
+        this.precio = precio;
+        this.nHab = nHab;
+        this.nBan = nBan;
+        this.precioM2 = precioM2;
+        this.superficie = superficie;
+        this.distrito = distrito;
+       
     }
-
-    public void recibirMensaje(ObjectMessage message) throws JMSException {
-        Object object = message.getObject();
-        if (object instanceof ArrayList) {
-            ArrayList<Inmueble> inmuebles = (ArrayList<Inmueble>) message.getObject();
-            for (Inmueble inmueble : inmuebles) {
-                Inmueble inmuebleCambiado = calcularPrecio(inmueble);
-                inmueblesCambiados.add(inmuebleCambiado);
-            }
-        }
-        else{
-            System.out.println("Error no se ha recibido un arraylist\n");
-        }
-        enviarInmueblesCambiados(null,inmueblesCambiados);
+    public Delegacion() {
+	}
+	//getters and setters
+    public int getPrecio() {
+        return precio;
     }
-
-    public Inmueble calcularPrecio(Inmueble inmueble) {
-        int precio = inmueble.getPrecio();
-        int numeroDormitorios = inmueble.getNumeroDormitorios();
-        int numeroBanos = inmueble.getNumeroBanos();
-        int superficie = inmueble.getSuperficie();
-        int precioFinal = inmueble.getPrecio()/inmueble.getSuperficie();
-        Inmueble inmuebleDelegacion = new Inmueble(inmueble.getDelegacion(), inmueble.getDistrito(), precio, numeroDormitorios, numeroBanos, superficie, precioFinal);
-        return inmuebleDelegacion;
+    public void setPrecio(int precio) {
+        this.precio = precio;
     }
-
-    private void enviarInmueblesCambiados(ObjectMessage msg, ArrayList<Inmueble> lista) throws JMSException{
-         // crear conexion con el servidor de mensajeria
-         msg.setObject(lista);
-         ConnectionFactory connectionFactory = new com.sun.messaging.ConnectionFactory();
-         Connection connection = null;
-         try {
-             connection = connectionFactory.createConnection();
-             connection.start();
-             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-             Destination destination = session.createQueue("Oficina");
-             MessageProducer producer = session.createProducer(destination);
-             TextMessage message = session.createTextMessage();
-             message.setText(msg.toString());
-             producer.send(message);
-             System.out.println("Mensaje enviado: " + message.getText());
-             session.close();
-             connection.close();
-         } catch (JMSException e) {
-             e.printStackTrace();
-         }
+    public int getnHab() {
+        return nHab;
+    }
+    public void setnHab(int nHab) {
+        this.nHab = nHab;
+    }
+    public int getnBan() {
+        return nBan;
+    }
+    public void setnBan(int nBan) {
+        this.nBan = nBan;
+    }
+    public int getSuperficie() {
+        return superficie;
+    }
+    public void setSuperficie(int superficie) {
+        this.superficie = superficie;
+    }
+    public String getDistrito() {
+        return distrito;
+    }
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+    public float getPrecioM2() {
+        return precioM2;
+    }
+    public void setPrecioM2(float precioM2) {
+        this.precioM2 = precioM2;
     }
 }
---
+++ jmsproject/src/main/java/com/jmsproject/EquipoGestor.java
@@ -0,0 +1,81 @@
+package com.jmsproject;
+
+import java.util.ArrayList;
+
+import javax.jms.*;
+
+public class EquipoGestor {
+	static Session mySess;
+	private static Connection myConn;
+	private Topic myTopic;
+	private MessageConsumer myConsumer;
+	private static String[] equiposGestores={"Negocio","Direccion"};
+	private static ArrayList<Delegacion> listaRecibidos;
+	// Los equipos gestores con consumidores de mensajes
+	// implementar DelegacionListener
+	// printear por pantalla
+
+	public static class DelegacionesListener implements MessageListener {
+		public boolean recibido = false;
+
+		@Override
+		public void onMessage(Message msg) {
+			if (msg instanceof ObjectMessage) {
+				recibido = true;
+				ObjectMessage objMsg = (ObjectMessage) msg;
+				try {
+					listaRecibidos = (ArrayList<Delegacion>) objMsg.getObject();
+					for (Delegacion del : listaRecibidos){
+						System.out.println("\tLeyendo el mensaje\n\t"
+							+ del.toString()); //FIXME debería printear nada más recibe el mensaje
+					}
+
+				} catch (Exception e) {
+					System.out.println("Exception in onMessage(): " + e.toString());
+				}
+			}
+		}
+	}
+
+	
+	public EquipoGestor (String equipoGestor){
+		listaRecibidos = new ArrayList<Delegacion>();
+		try {
+			// Create a JMS topic
+			myTopic = mySess.createTopic(equipoGestor); 
+			// Create a JMS publisher and subscriber
+			myConsumer = mySess.createConsumer(myTopic);
+			// Set a JMS message listener and include the class that will handle the message
+			EquipoGestor.DelegacionesListener myListener = new DelegacionesListener();
+			myConsumer.setMessageListener(myListener);
+			myConn.start();
+			// TODO MIENTRAS NO SE HAYA RECIBIDO EL MENSAJE
+			while(!myListener.recibido){
+				System.out.println("\tEsperando datos por 100 ms ... ");
+				Thread.sleep(100);
+			}
+		} catch (JMSException e) {
+			System.out.println("Error al inicializar JMS");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	// escribir constructor meter el main en la clase y hacer un main q inicialice 2 equipos gestores: direccion y negocio
+
+	public static void main(String[] args) throws JMSException {
+			// Crea una connection factory
+			com.sun.messaging.ConnectionFactory connectionFactory = new com.sun.messaging.ConnectionFactory();
+			// Crea una JMS connection
+			myConn = connectionFactory.createConnection();
+			// Crea una JMS session
+			mySess = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			//Crea equipoGestor
+			for (String equipoGestor : equiposGestores) {
+				new EquipoGestor(equipoGestor);
+			}
+			myConn.close();
+			mySess.close();
+
+	}
+
+}
