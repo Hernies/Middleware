49c43cb79c2911e4a8a6e3c476da79bfb9528f34
Hernies <hernancalvo>
2023-06-26 14:23:34 +0200

    launch before debugging (already in tasks)

--- debug_setup.sh
+++ debug_setup.sh
@@ -3,7 +3,7 @@
 # Check if OpenMQ server is running
 if pgrep -x "imqbrokerd" >/dev/null; then
   echo "OpenMQ server is already running."
-  mvn compile
+  cd jmsproject/ && mvn compile
 else
   echo "OpenMQ server is not running. Launching the server..."
   screen -dmS imq sh -c "imqbrokerd -tty; exec"
